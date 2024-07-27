3a3c1e180ac475d52fff3cd85a5fcea8215ec073
masashi2000 <s12701350406@gmail.com>
2024-07-27 17:16:50 +0900

    ペルソナファイルのカラム名を変更した。

--- status_update_task/5_no_injection_persona/prompt/generate_no_injected_50_prompts.py
+++ status_update_task/5_no_injection_persona/prompt/generate_no_injected_50_prompts.py
@@ -18,7 +18,7 @@ def generate_2250_prompts():
 
     # 'persona'列の各レコードの先頭に "A:" を追加
     df['persona'] = 'For the following task, respond in a way that matches this description: "' + df['persona'] + '"' + " Generate a list of 20 different Facebook status updates as this person. Each update must be verbose and reflect the person’s character and description. The updates should cover, but should not be limited to, the following topics: work, family, friends, free time, romantic life, TV / music / media consumption, and communication with others."
-
+    df.rename(columns={'persona': 'prompt'}, inplace=True)
     # 修正されたデータフレームを新しいCSVファイルとして保存
     output_path = 'persona_50_prompts_without_injection.csv'  # 保存先のパスに置き換えてください
     df.to_csv(output_path, index=False)
